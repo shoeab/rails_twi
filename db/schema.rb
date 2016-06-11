40d484bc22c902539bea2f982dfeb3717459d6e9
Shoeab Ahmed <shoeabahmed4@gmail.com>
2016-06-11 16:43:10 +0600

    users picture issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610024632) do
+ActiveRecord::Schema.define(version: 20140725142845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20160610024632) do
     t.datetime "updated_at"
     t.string   "remember_token"
     t.string   "slug"
-    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
